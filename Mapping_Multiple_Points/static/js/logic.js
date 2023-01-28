76e3659390fc2a9e551f126ea4ad2628c428345f
RyanDevillier <ryancdevillier@gmail.com>
2023-01-28 00:20:17 -0600

    Adding map files that generate multiple markers

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -2,14 +2,24 @@
 console.log("Working.");
 
 // Creating the map object with a center ([lat, lng]) and zoom level
-let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+let map = L.map('mapid').setView([40.7, -94.5], 4);
 
-// Adding a marker to the map for Los Angeles, California
-let marker = L.circleMarker([34.0522, -118.2437], {
-    radius: 300,
-    color: "black",
-    fillColor: "#ffffa1"
-}).addTo(map);
+// An array containing each city's location, state, and population.
+let cityData = cities;
+
+// Looping through the cities array and creating one marker for each city
+cityData.forEach(function(city) {
+    console.log(city);
+    L.circleMarker(
+        city.location,
+        {
+        radius: city.population/200000,
+        color: "#ffa500",
+        weight: 4
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
+});
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
